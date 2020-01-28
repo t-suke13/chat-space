1f3371c0edb2325131e0aef7da8484b6241c69d9
t-suke13 <taichanchan13@gmail.com>
2020-01-28 19:50:52 +0900

    自動更新機能実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -53,7 +53,7 @@ $(function(){
       contentType: false
     })
       .done(function(data){
-        console.table(data)
+        // console.table(data)
         var html = buildHTML(data);
         $('.chat-main__message-list').append(html);
         $('form')[0].reset();
@@ -64,6 +64,37 @@ $(function(){
       .fail(function() {
         alert("メッセージ送信に失敗しました");
       });
-
+    
   })
+
+  var reloadMessages = function() {
+    
+   var last_message_id = $('.message:last').data("message-id");
+   console.log(last_message_id);
+    $.ajax({
+      
+      url: "api/messages",      
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.chat-main__message-list').append(insertHTML);
+        $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+        $("#new_message")[0].reset();
+        $(".form__submit").prop("disabled", false);
+      }
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });

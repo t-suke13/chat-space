3ac0ef44d654f2f0dfcf6031a0735ba9f315ba86
t-suke13 <taichanchan13@gmail.com>
2020-01-30 14:59:17 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,10 +4,10 @@ worker_processes 1
 
 working_directory "#{app_path}/current"
 
-listen "#{app_path}/nessages/tmp/sockets/unicorn.sock"
-pid "#{app_path}/messages/tmp/pids/unicorn.pid"
-stderr_path "#{app_path}/messages/log/unicorn.stderr.log"
-stdout_path "#{app_path}/messages/log/unicorn.stdout.log"
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 
 #Railsアプリケーションの応答を待つ上限時間を設定
